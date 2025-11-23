0e790ab7aedbfce466748bea1dde7ec2c03bb626
atharvamarne7 <atharvamarne26@gmail.com>
2025-11-23 23:26:42 +0530

    Add pattern printing pograms for Assignment_28
    Includes pattern printing program :
    - Linear pattern 
    Demonstrates simple logic implementation using nested loops

---
+++ ASSIGNMENTS/Assignment_28/Assignment_28/program28_5.java
@@ -0,0 +1,49 @@
+/* Accept two inputs from user and print the pattern
+
+    Input : iRow = 4
+            iCol = 4
+
+    Output :  
+*/ 
+
+import java.util.*;
+
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+        int iNum = 1;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print(iNum+"\t");
+                iNum++;
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program28_5
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue1,iValue2);
+
+    }
+}
