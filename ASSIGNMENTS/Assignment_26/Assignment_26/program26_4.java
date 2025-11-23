fba93afc65aaf2be26ecce7f2f43ecfdd3e7e54c
atharvamarne7 <atharvamarne26@gmail.com>
2025-11-23 23:20:18 +0530

    Add pattern printing programs for Assignment_26
    Includes pattern printing program :
    - Linear pattern 
    Demonstrates simple logic implementation using nested loops

---
+++ ASSIGNMENTS/Assignment_26/Assignment_26/program26_4.java
@@ -0,0 +1,38 @@
+/*  Accept the number from user and display the below pattern
+
+    Input : 5
+    Output : #  1  *   #    2   *   #   3  *    #   4   *   #   5   *
+
+*/
+
+import java.util.*;
+
+class pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+        
+
+        for(iCnt = 1 ; iCnt <= iNo; iCnt++)
+        {
+            System.out.printf("#\t%d\t*\t",iCnt);
+        }
+        System.out.println();
+    }
+}
+
+class program26_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue = 0;
+
+        System.out.println("Enter the number : ");
+        iValue = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue);
+    }
+}
