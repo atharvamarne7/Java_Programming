a60a5d970ee6833d137c426f84fe46aaeb40110d
atharvamarne7 <atharvamarne26@gmail.com>
2025-11-25 22:58:41 +0530

    Add files via upload

---
+++ ASSIGNMENTS/Assignment_32/program32_4.java
@@ -0,0 +1,69 @@
+/*
+    iRow = 6
+    iCol = 6
+
+    output: 
+*       *       *       *       *       *
+*       #       #       #       *       *
+*       #       #       *       $       *
+*       #       *       $       $       *
+*       *       $       $       $       *
+*       *       *       *       *       *
+
+*/
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                 // Border
+                if(i == 1 || i == iRow || j == 1 || j == iCol)
+                {
+                    System.out.print("*\t");
+                }
+                // Opposite diagonal (# and $ depend on position)
+                else if(j == (iCol - i + 1))
+                {
+                    System.out.print("*\t");   // diagonal boundary also *
+                }
+                // Left side of diagonal  #
+                else if(j < (iCol - i + 1))
+                {
+                    System.out.print("#\t");
+                }
+                // Right side of diagonal  $
+                else
+                {
+                    System.out.print("$\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program32_4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+    }
+}
